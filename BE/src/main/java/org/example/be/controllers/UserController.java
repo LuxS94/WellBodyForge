c9e544db100448968077fb6f60afadd6c2a55123
LuxS94 <simonetti.lucio@libero.it>
2026-03-09 12:11:57 +0100

    updated UserController

--- BE/src/main/java/org/example/be/controllers/UserController.java
+++ BE/src/main/java/org/example/be/controllers/UserController.java
@@ -3,12 +3,17 @@ package org.example.be.controllers;
 import org.example.be.dto.UserDTO;
 import org.example.be.entities.User;
 import org.example.be.entities.UserSecurity;
+import org.example.be.exceptions.ValidationException;
 import org.example.be.services.UserService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -26,6 +31,30 @@ public class UserController {
     public User showMyProfile(@AuthenticationPrincipal UserSecurity currentUser) {
         return this.us.findById(currentUser.getId());
     }//http://localhost:3001/user/myProfile
+
+    @GetMapping("/all")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Page<User> showsAllUsers(@RequestParam(defaultValue = "0") int page,
+                                    @RequestParam(defaultValue = "10") int size,
+                                    @RequestParam(defaultValue = "date") String orderBy,
+                                    @RequestParam(defaultValue = "asc") String sortCriteria) {
+        return this.us.findAllUsers(page, size, orderBy, sortCriteria);
+    }//http://localhost:3001/user/all
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User create(UserDTO body, BindingResult validationResults) {
+        if (validationResults.hasErrors()) {
+
+            List<String> errorsList = validationResults.getFieldErrors()
+                    .stream().map(fieldError -> fieldError.getDefaultMessage()).toList();
+
+            throw new ValidationException(errorsList);
+        } else {
+            return this.us.create(body);
+        }
+    }
 
     @PutMapping("/updateMe")
     public User updateMyProfile(@AuthenticationPrincipal UserSecurity currentUser, @RequestBody UserDTO body) {
