8404ac836139521d16832465b5a6a1c04850de2c
LuxS94 <simonetti.lucio@libero.it>
2026-03-11 18:20:58 +0100

    home

--- FE/src/assets/components/Home.jsx
+++ FE/src/assets/components/Home.jsx
@@ -1,14 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form } from 'react-bootstrap';
+import { Col, Form } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+
 
 function Home() {
     return(<>
     <h1 className='welcome'>Welcome</h1>
-    <div style={{backgroundColor:'white'}} className='mt-5 rounded-3 welcome2'><h2 style={{color:'#FC7E00'}}>Start now with WellBodyForge</h2><h3 className='fw-bolder mt-5'>INSERT YOUR DATA</h3> <Form.Group className="m-3"  controlId="formBasicEmail">
-        <Form.Label >Email address</Form.Label>
-        <Form.Control className="m-3 d-inline" style={{width:'300px'}}  type="email" placeholder="Enter email" />
-        <Form.Label>Password</Form.Label>
-        <Form.Control className="m-3 d-inline" style={{width:'300px'}} type="password" placeholder="Password" /> </Form.Group></div>
+    <div style={{backgroundColor:'white'}} className='mt-5 rounded-3 welcome2'><h2 style={{color:'#FC7E00'}}>Start now with WellBodyForge</h2><h3 className='fw-bolder mt-5'>INSERT YOUR DATA</h3><Form className='container mt-4'><Row > <Form.Group className=""  controlId="formBasicEmail">
+        <Col className='d-inline xs={12} m={4} lg={3}'>
+        <Form.Control className="m-2 d-inline me-lg-5 ms-lg-5 mt-2 " style={{ maxWidth: '280px'}} type="text" placeholder="Choose your username" /></Col>
+         <Col className='d-inline xs={12} m={4} lg={3}'>
+        <Form.Control className="m-2 d-inline me-lg-5 ms-lg-5 mt-2 " style={{ maxWidth: '280px'}} type="email" placeholder="Enter email" /></Col>
+        <Col className='d-inline xs={12} m={4} lg={3}'>
+        <Form.Control className="m-2 d-inline me-lg-5 ms-lg-5 mt-2" style={{ maxWidth: '280px'}}type="password" placeholder="*Choose your password" /> <Form.Label className='d-block mt-2' style={{color:'grey'}}>*The password must be at least 8 characters long and include at least one uppercase letter, one number, and one special character.</Form.Label> </Col></Form.Group></Row></Form></div>
     </>)
 }
 export default Home
--- FE/src/assets/components/MyNav.jsx
+++ FE/src/assets/components/MyNav.jsx
@@ -11,7 +11,6 @@ function MyNav() {
          
           <Nav className="me-auto,justify-content-center" > <Navbar.Brand style={{padding:'0'}} as={Link} to='/' ><img style={{height:'50px',padding:'0'}} src='src\assets\components\img\unnamed.jpg'></img></Navbar.Brand>
             <Nav.Link className='align-content-center fw-bolder' as={Link} to='/'>Home</Nav.Link>
-            <Nav.Link className='align-content-center fw-bolder'as={Link} to=''>Pricing</Nav.Link>
              <Nav.Link className='align-content-center fw-bolder'as={Link} to='/learnMore'>More</Nav.Link>
           </Nav>
           <Nav.Link id='login' style={{height:'50px'}} className='align-content-center' as={Link} to=''><i className="bi bi-person-circle me-3 mb-1"></i>LogIn</Nav.Link>
