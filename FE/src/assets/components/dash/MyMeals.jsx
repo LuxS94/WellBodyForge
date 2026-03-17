6e608e0d07bb5956cba2b925c532c550a3087773
LuxS94 <simonetti.lucio@libero.it>
2026-03-17 11:38:47 +0100

    add meal

--- FE/src/assets/components/dash/MyMeals.jsx
+++ FE/src/assets/components/dash/MyMeals.jsx
@@ -3,22 +3,25 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
+import {Form}  from 'react-bootstrap';
 function MyMeals() {
- const [meals,setMeals]=useState([]);   
-   const [date, setDate] = useState(new Date());
+ const [meals,setMeals]=useState([]);  
+ const [dates, setDates] = useState(new Date()); 
+ const[adMeal,setAdmeal]=useState({description:'',date:dates.toISOString().split("T")[0]})
+    const [showForm, setShowForm] = useState(false);
   const Next = () => {
-    const nextDate = new Date(date);
+    const nextDate = new Date(dates);
     nextDate.setDate(nextDate.getDate() + 1);
-    setDate(nextDate);
+    setDates(nextDate);
   };
 const Prev = () => {
-    const prevDate = new Date(date);
+    const prevDate = new Date(dates);
     prevDate.setDate(prevDate.getDate() - 1);
-    setDate(prevDate);
+    setDates(prevDate);
   };
      const fmeal=()=>{
 const port=import.meta.env.VITE_PORT;
-fetch(`http://localhost:${port}/meals/my?date=${date.toLocaleDateString("en-CA")}`, {
+fetch(`http://localhost:${port}/meals/my?date=${dates.toLocaleDateString("en-CA")}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -30,26 +33,70 @@ fetch(`http://localhost:${port}/meals/my?date=${date.toLocaleDateString("en-CA")
         {return data;} else {throw new Error('Error in response')}})
     .then((data) => {setMeals(data.content)})
     .catch(err=>console.log(err.message));  }
+    const addMeal=(e)=>{e.preventDefault();
+      const body = {
+    description: adMeal.description,
+    date: dates.toISOString().split("T")[0] 
+  };
+      const port=import.meta.env.VITE_PORT;
+    fetch(`http://localhost:${port}/meals`,{
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+       }, body:JSON.stringify(body)
+    })
+    .then(async(res)=>{ const data = await res.json();
+        if(res.ok) 
+        {return data;} else {throw new Error('Error in response')}})
+    .then(data=>{setMeals(prev => [...prev, data]); 
+    setAdmeal({ description: '' });setShowForm(false);})
+   .catch(err=>console.log(err.message)); 
+    }
  useEffect(() => { 
-   
     fmeal();
-  }, [date]);
+  }, [dates]);
   
   return (
     <>
     <h1 className="welcome">My Meals</h1>
        <div style={{backgroundColor:'white',textAlign:'center'}} className='mt-5 welcome2 rounded-3' >
-        <div className='pt-3 ms-3 w-100'>
+        <div className='pt-3 ms-3 w-100 '>
       <i onClick={Prev} id='prev' class="bi bi-caret-left-fill"style={{color:'black'}}></i> <input
   type="date"
-  value={date.toLocaleDateString("en-CA")}
-  onChange={(e) => setDate(new Date(e.target.value))}
+  value={dates.toLocaleDateString("en-CA")}
+  onChange={(e) => setDates(new Date(e.target.value))}
 />
  <i onClick={Next} id='next' style={{color:'black'}}class="bi bi-caret-right-fill"></i></div>
       
-       <Button  className= "text-center border-0 mt-4 mb-4 ms-3" style={{background:'#FC7E00'}}  size="lg">Add Meal</Button>
+       <Button  onClick={() => setShowForm(!showForm)} className= "text-center border-0 mt-4 mb-4 ms-4" style={{background:'#FC7E00'}}  size="lg">Add Meal</Button>
+       {showForm && (<div className='d-flex justify-content-center ' style={{zIndex: 9999, position: "sticky",
+            top: 325}}>
+        <Card className='w-75'
+          style={{
+            marginTop: "20px",
+            maxWidth: "300px",
+            padding: "15px",
+            backgroundColor: "#fff",
+          }}
+        >
+          <h5>Inserisci i dati</h5>
+          <Form onSubmit={addMeal}>
+            <Form.Group className="mb-3" controlId="exampleEmail">
+              <Form.Label>Description</Form.Label>
+              <Form.Control   onChange={(e) =>{
+    setAdmeal(prev => ({ ...prev, description: e.target.value }))}
+  } name='description' as="textarea" rows={3} placeholder="Insert meal description" />
+            </Form.Group>
+          
+            <Button type="submit"  style={{ background: "#FC7E00", border: "none" }}>
+              Add
+            </Button>
+          </Form>
+        </Card></div>
+      )}
  {meals.map(meal => (
-    <Card style={{maxWidth:'300px'}} key={meal.id} className="mb-3 ms-3 mt-2">
+    <Card style={{maxWidth:'300px',maxHeight:'500px'}} key={meal.id} className="mb-3 ms-4 mt-2 border-black">
       <Card.Body>
         <Card.Title >{meal.description}</Card.Title>
         <Card.Footer>{meal.mealFoods.map(f => (
