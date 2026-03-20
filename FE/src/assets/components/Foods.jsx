726940a82f924860f07cfc95c82d79e8eb22be68
LuxS94 <simonetti.lucio@libero.it>
2026-03-20 17:11:45 +0100

    update foods

--- FE/src/assets/components/Foods.jsx
+++ FE/src/assets/components/Foods.jsx
@@ -4,13 +4,33 @@ import {Form } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import useUser from "../script/useUser";
+import { useNavigate } from 'react-router-dom'; 
 function Foods (){
   const user=useUser()
+  const navigate=useNavigate();
     const[food,setFood]=useState([])
      const [search, setSearch] = useState('');
      const [showForm, setShowForm] = useState(false);
+     const[showUpForm,setShowUpForm]=useState(false);
+     const[updateFood,setUpdateFood]=useState([])
      const [newFood, setNewFood] = useState({ name: '', type: '', kcal: '', protein: '', carbs: '', fat: '' });
     const port=import.meta.env.VITE_PORT;
+       const filteredFood = food.filter(f =>           //filter for searchbar
+    f.name.toLowerCase().includes(search.toLowerCase())
+  );  
+  const handleEditClick = (foodItem) => {     //initializes all fields, necessary for calories/kcal
+  setUpdateFood({
+    id: foodItem.id,
+    name: foodItem.name,
+    type: foodItem.type,
+    kcal: foodItem.calories,   
+    protein: foodItem.protein,
+    carbs: foodItem.carbs,
+    fat: foodItem.fat
+  });
+  setShowUpForm(true);
+}
+  // add food-----------------------------------------------------------------------------------------------------------
     const handleAddFood = () => {
       fetch(`http://localhost:${port}/food/f/add`,{
  method:'POST',
@@ -23,10 +43,34 @@ function Foods (){
         if(res.ok) 
         {return data;} else {throw new Error(data.message || data.error || "Error in response")}})
     .then(data=>{setNewFood(data);setShowForm(false);setNewFood({ name: '', type: '', kcal: '', protein: '', carbs: '', fat: '' });gfood();})}
-  
-     const filteredFood = food.filter(f =>
-    f.name.toLowerCase().includes(search.toLowerCase())
-  );
+    // update food----------------------------------------------------------------------------------------------------------
+ 
+    const handleUpdateFood=(id)=>{
+    fetch(`http://localhost:${port}/food/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${localStorage.getItem("token")}`
+    },
+    body: JSON.stringify(updateFood)
+  })
+    .then(async res => {
+      const data = await res.json();
+      if (res.ok) return data;
+      else throw new Error(data.message || data.error || "Error in response");
+      
+    })
+    .then(() => {
+  gfood();
+  setShowUpForm(false);
+})
+      .catch(err => {
+      console.log(err.message);
+      navigate("/error", { state: { message: err.message } });
+    })
+  }
+
+  // get all foods------------------------------------------------------------------------------------------------------
   const gfood=()=>{
 fetch(`http://localhost:${port}/food/f/all?page=0&size=1000`, {
     headers: {
@@ -160,11 +204,108 @@ fetch(`http://localhost:${port}/food/f/all?page=0&size=1000`, {
       <th scope="col">Protein</th>
       <th scope="col">Carbs</th>
       <th scope="col">Fat</th>
+      {user.role==='ADMIN'?(<><td></td></>):null}
     </tr>
   </thead>
    <tbody>{filteredFood.map((f,index)=>(
-   <tr key={index}> <th>{index+1}</th><td>{f.name}</td><td>{f.type}</td><td>{f.calories}</td><td>{f.protein}</td><td>{f.carbs}</td><td>{f.fat}</td></tr>
+   <tr key={f.id}> <th>{index+1}</th><td>{f.name}</td><td>{f.type}</td><td>{f.calories}</td><td>{f.protein}</td><td>{f.carbs}</td><td>{f.fat}</td>{user.role==='ADMIN'?(<><td><i onClick={()=>{ handleEditClick(f);}} class="bi bi-pencil-square me-3 uf"></i><i class="bi bi-trash3 uf"></i></td></>):null}</tr>
    ))}</tbody>
-  </table></div> </>)
+  </table>
+  {/* food-update form ---------------------------------------------------------------------------------------------------- */}
+  {showUpForm && ( <div style={{
+          position: 'fixed',
+          top: 30, left: 0, width: '100%', height: '100%',
+          backgroundColor: 'rgba(0,0,0,0.5)',
+          display: 'flex', justifyContent: 'center', alignItems: 'center',
+          zIndex: 9999
+        }}>
+          <div style={{
+            textAlign:'center',
+            backgroundColor: 'white',
+            padding: '20px',
+            borderRadius: '10px',
+            minWidth: '300px',
+            maxWidth: '500px'
+          }}>
+            <h4 style={{color:'black'}}>Update Food</h4>
+            <Form>
+              <Form.Group className="mb-3">
+    <Form.Label style={{color:'black'}}>Name</Form.Label>
+    <Form.Control
+      type="text"
+      value={updateFood.name}
+      onChange={e => setUpdateFood({ ...updateFood, name: e.target.value })}
+      step="0.01"
+    />
+  </Form.Group>
+
+  <Form.Group className="mb-3">
+  <Form.Label style={{color:'black'}}>Type</Form.Label>
+  <Form.Select
+    value={updateFood.type}
+    onChange={e => setUpdateFood({ ...updateFood, type: e.target.value })}
+  >
+    <option value="">Select type</option>
+    <option value="ANIMAL">ANIMAL</option>
+    <option value="FRUIT">FRUIT</option>
+    <option value="INDUSTRIAL">INDUSTRIAL</option>
+    <option value="VEGETABLE">VEGETABLE</option>
+    <option value="OTHER">OTHER</option>
+  </Form.Select>
+</Form.Group>
+
+  <Form.Group className="mb-3">
+    <Form.Label style={{color:'black'}}>Calories</Form.Label>
+    <Form.Control
+      type="number"
+      value={updateFood.kcal}
+      onChange={e => setUpdateFood({ ...updateFood, kcal: e.target.value })}
+      step="0.01"
+    />
+  </Form.Group>
+
+  <Form.Group className="mb-3">
+    <Form.Label style={{color:'black'}}>Protein</Form.Label>
+    <Form.Control
+      type="number"
+      value={updateFood.protein}
+      onChange={e => setUpdateFood({ ...updateFood, protein: e.target.value })}
+      step="0.01"
+    />
+  </Form.Group>
+
+  <Form.Group className="mb-3">
+    <Form.Label style={{color:'black'}}>Carbs</Form.Label>
+    <Form.Control
+      type="number"
+      value={updateFood.carbs}
+      onChange={e => setUpdateFood({ ...updateFood, carbs: e.target.value })}
+      step="0.01"
+    />
+  </Form.Group>
+
+  <Form.Group className="mb-3">
+    <Form.Label style={{color:'black'}}>Fat</Form.Label>
+    <Form.Control
+      type="number"
+      value={updateFood.fat}
+      onChange={e => setUpdateFood({ ...updateFood, fat: e.target.value })}
+      step="0.01"
+    />
+  </Form.Group>
+
+  <div className="d-flex justify-content-end">
+    <Button style={{background:'#FC7E00',width:'73px'}} className="me-3 border-0" onClick={()=>handleUpdateFood(updateFood.id)}>
+      Update
+    </Button> <Button variant="danger" className="me-2" style={{width:'73px'}} onClick={() => setShowUpForm(false)}>
+      Cancel
+    </Button>
+  </div>
+            </Form>
+          </div>
+        </div>
+
+   )}
+   </div> </>)
 }
 export default Foods
