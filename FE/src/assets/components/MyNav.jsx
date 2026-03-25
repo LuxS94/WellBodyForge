79aa986e292ac3a7d30a563291fe25ae7a49c47b
LuxS94 <simonetti.lucio@libero.it>
2026-03-25 18:30:07 +0100

    imgs

--- FE/src/assets/components/MyNav.jsx
+++ FE/src/assets/components/MyNav.jsx
@@ -14,7 +14,7 @@ function MyNav() {
  <Navbar expand="md" sticky="top" style={{backgroundColor: '#ffffff',padding:'0',marginTop:'15px'}} data-bs-theme="light">
         <Container  >
          
-           <Navbar.Brand style={{padding:'0'}} as={Link} to={username ? '/homeLog' : '/'} ><img style={{height:'50px',padding:'0'}} src='src\assets\components\img\unnamed.jpg'></img></Navbar.Brand>
+           <Navbar.Brand style={{padding:'0'}} as={Link} to={username ? '/homeLog' : '/'} ><img style={{height:'50px',padding:'0'}} src='\logo.jpg'></img></Navbar.Brand>
 {/*mobile version--------------------------------------------------------------------------------------------------  */}
             <Nav.Link id='login' style={{height:'50px'}} className='align-content-center d-flex justify-content-center d-md-none align-items-center' as={Link} to={username ? '/profile' : '/login'}>{username 
             ?( <><i className="bi bi-person-circle me-2 mb-1"></i>{username}<span className='d-inline ms-3' style={{color:'#c41b1b'}}  onClick={(e)=>{ e.stopPropagation();logout()}}>LogOut</span></>)
